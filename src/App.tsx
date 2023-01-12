f7a930d3cc40776123d2bb37440ce83a84e6500c
LeonardoPaiv <leopaiva45@gmail.com>
2023-01-12 12:33:16 -0300

    task 3 improvement

--- src/App.tsx
+++ src/App.tsx
@@ -20,26 +20,13 @@ function App() {
     })
   }
 
-  const filterCols = (event: any) => {
-    if (event.target.value) {
-      const colsFiltred = dummyCols.filter(col => col.amount == event.target.value)
-      setdummyCols(colsFiltred)
-    }
-    if (!event.target.value) {
-      setdummyCols(cols)
-    }
-  }
-
   return (
     <div className="grid flex flex-column text-400 font-semibold md:flex-row">
       <div className="col md:col-4">
         <NewCol onAddCol={addColHandler}/>
       </div>
       <div className="col md:col-8">
-        <div className="grid">
-          <div className="col-12 w-full flex justify-content-end"><label htmlFor="filter">Filter:</label><input type="number" onChange={filterCols}/></div>
-          {dummyCols.map(col => <Cols key={col.id} title={col.title} amount={col.amount} />)}
-        </div>
+        <Cols cols={dummyCols}/>
       </div>
     </div>
   )
--- src/components/Cols.tsx
+++ src/components/Cols.tsx
@@ -2,16 +2,30 @@ import React, { useState } from 'react'
 
 const Cols = (props: any) => {
 
-  const [title, setTitle] = useState(props.title);
-  const [amount, setAmount] = useState(props.amount);
-  const [id, setId] = useState(props.id);
+  const dummyCols: any[] = props.cols;
+
+  const [filter, setFilter] = useState('1')
+
+  const filterChangeHandler = (event: any) => {
+    setFilter(event.target.value)
+  }
+
+  const filteredCols = dummyCols.filter(col => col.amount == filter)
 
   return (
-    <div className="col-12 md:col-4">
-        <div key={props.id} className='surface-600 p-2 border-round-md flex justify-content-between'>
-          <h2 className='text-xl'>{title}</h2>
-          <h2 className='text-xl'>{amount}</h2>
+    <div className='grid'>
+      <div className="col-12 w-full flex justify-content-end">
+        <label htmlFor="filter">Filter:</label>
+        <input type="number" value={filter} onChange={filterChangeHandler}/>
+      </div>
+      {filteredCols.map((col, index) => {
+        return <div key={index} className='col-12 md:col-4'>
+          <div key={col.id} className='surface-600 p-2 border-round-md flex justify-content-between'>
+          <h2 className='text-xl'>{col.title}</h2>
+          <h2 className='text-xl'>{col.amount}</h2>
+          </div>
         </div>
+      })}
     </div>
   )
 }
